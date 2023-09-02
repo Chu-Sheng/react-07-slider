5d1a1a9ff31f0da33fa7802ce2b14bc36777d219
Chu-Sheng <cooper.juan@yahoo.com>
2023-09-02 14:19:54 -0400

    app updated

--- src/App.js
+++ src/App.js
@@ -4,7 +4,8 @@ import { FaQuoteRight } from 'react-icons/fa';
 import data from './data';
 
 function App() {
-  const [people, setPeople] = useState(data);
+  // const [people, setPeople] = useState(data);
+  const people = data;
   const [index, setIndex] = useState(0)
   const checkNumber = (number) => {
     if (number > people.length - 1) {
@@ -25,8 +26,25 @@ function App() {
     return checkNumber(newIndex);
   }
 
-  // console.log(people[index])
-  // const {id, image, name, quote, title} = people[index]
+  useEffect(() => {
+    const lastIndex = people.length - 1;
+    if (index < 0) {
+      setIndex(lastIndex);
+    }
+    if (index > lastIndex) {
+      setIndex(0);
+    }
+  }, [index, people]);
+
+  useEffect(() => {
+    let slider = setInterval(() => {
+      setIndex(index + 1);
+    }, 5000);
+    return () => {
+      clearInterval(slider);
+    };
+  }, [index]);
+
   return (
     <section className="section">
       <div className="title">
