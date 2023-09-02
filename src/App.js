be50527f86a30f4f21ab05c92a0c6502839447cf
Chu-Sheng <cooper.juan@yahoo.com>
2023-09-02 13:47:30 -0400

    initial commit

--- src/App.js
+++ src/App.js
@@ -1,25 +1,68 @@
-import logo from './logo.svg';
-import './App.css';
+import React, { useState, useEffect } from 'react';
+import { FiChevronRight, FiChevronLeft } from 'react-icons/fi';
+import { FaQuoteRight } from 'react-icons/fa';
+import data from './data';
 
 function App() {
+  const [people, setPeople] = useState(data);
+  const [index, setIndex] = useState(0)
+  const checkNumber = (number) => {
+    if (number > people.length - 1) {
+      return setIndex(0)
+    }
+    if (number < 0) {
+      return  setIndex(people.length - 1)
+    }
+    return setIndex(number)
+  }
+
+  const prevPerson = () => {
+    let newIndex = index - 1;
+    return checkNumber(newIndex);
+  }
+  const nextPerson = () => {
+    let newIndex = index + 1;
+    return checkNumber(newIndex);
+  }
+
+  // console.log(people[index])
+  // const {id, image, name, quote, title} = people[index]
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+    <section className="section">
+      <div className="title">
+        <h2> <span>/</span> reviews </h2>
+      </div>
+      <div className="section-center">
+        {people.map((person, personIndex) => {
+          const {id, image, name, quote, title} = person
+          let position = 'nextSlide';
+          if (personIndex === index) {
+            position = 'activeSlide'
+          }
+          if (personIndex === index - 1 ||
+            (index === 0 && personIndex === people.length - 1)) {
+              position = 'lastSlide'
+          } 
+          return (
+            <article className={position} key={id}>
+              <img className="person-img" src={image} alt={name}/>
+              <h4>{name}</h4>
+              <p className="title">{title}</p>
+              <p className="text">{quote}</p>
+              <FaQuoteRight className="icon"/>
+            </article>
+          )
+        })}
+        <button className="prev" onClick={prevPerson}>
+          <FiChevronLeft />
+        </button>
+        <button className="next" onClick={nextPerson}>
+          <FiChevronRight />
+        </button>
+
+      </div>
+    </section>
+  )
 }
 
-export default App;
+export default App
